42a46d61f6b30c607c3a494598e13cabe00f9873
ahngbeom <bbu0704@gmail.com>
2022-03-31 16:34:19 +0900

    minirt_close - (void)scene

--- srcs/hooks/minirt_close.c
+++ srcs/hooks/minirt_close.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minirt_close.c                                     :+:      :+:    :+:   */
+/*   miniRT_CLOSE.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 14:17:00 by bahn              #+#    #+#             */
-/*   Updated: 2022/03/31 16:26:11 by bahn             ###   ########.fr       */
+/*   Updated: 2022/03/31 16:33:13 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int	minirt_close(int keycode, t_scene *scene)
 {
-	// (void)scene;
+	(void)scene;
 	// if (scene != NULL)
 	// {
 		// mlx_destroy_image(scene->vars->mlx, scene->vars->img_data->img);
 		// mlx_destroy_window(scene->vars->mlx, scene->vars->win);
 		// nullcheck_free(scene->vars->mlx);
 	// }
-	// system("leaks --exclude --atExit scene");
+	system("leaks miniRT");
 	if (keycode != -1)
 		exit(0);
 	else
