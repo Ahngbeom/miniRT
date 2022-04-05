6fe4d81adc131e918e96037a9673d70e4b90bfbd
ahngbeom <bbu0704@gmail.com>
2022-04-06 00:31:45 +0900

    Set the camera coodinate using camera direction vector

--- srcs/camera/camera.c
+++ srcs/camera/camera.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 18:11:41 by bahn              #+#    #+#             */
-/*   Updated: 2022/04/06 00:21:08 by bahn             ###   ########.fr       */
+/*   Updated: 2022/04/06 00:29:19 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ void	init_camera(t_scene *scene, int fov)
 	// 카메라 좌표계 설정. 기저벡터?
 	scene->camera.vup = vector_init(0, 1, 0); // 카메라 기울기
 	scene->camera.w = vunit(vmul_t(-1.0, scene->camera.dir));	// 카메라 좌표계 z축. 카메라와 뷰포트의 거리 단위 벡터
-	scene->camera.u = vunit(vcross(scene->camera.vup, scene->camera.w)); // 카메라 좌표계 x축. vup과 w의 외적 : vup 벡터와 w 벡터의 수직 벡터
-	scene->camera.v = vcross(scene->camera.w, scene->camera.u);	// 카메라 좌표계 y축. w와 u의 외적 : w 벡터와 u 벡터의 수직 벡터를 카메라의 y축으로 잡는다.
+	scene->camera.u = vunit(vcross(scene->camera.vup, scene->camera.w)); // 카메라 좌표계 x축. vup과 w의 외적 : vup 벡터와 w 벡터의 수직 벡터를 카메라 좌표계의 x축으로 잡는다.
+	scene->camera.v = vcross(scene->camera.w, scene->camera.u);	// 카메라 좌표계 y축. w와 u의 외적 : w 벡터와 u 벡터의 수직 벡터를 카메라 좌표계의 y축으로 잡는다.
 	scene->camera.horizontal = vmul_t(scene->camera.viewport_width, scene->camera.u); // x축 벡터 좌표에 따라 뷰포트 수평 길이 벡터 결정
 	scene->camera.vertical = vmul_t(scene->camera.viewport_height, scene->camera.v); // y축 벡터 좌표에 따라 뷰포트 수직 길이 벡터 결정
 
