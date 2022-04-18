0a8a14c895e808dda96d4482234e0bc7027bf9dd
ahngbeom <bbu0704@gmail.com>
2022-04-18 19:42:36 +0900

    [Mac] Keybord Hook - Arrow key

--- srcs/hooks/camera_movement.c
+++ srcs/hooks/camera_movement.c
@@ -6,7 +6,7 @@
 /*   By: bahn <bahn@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 16:24:38 by bahn              #+#    #+#             */
-/*   Updated: 2022/04/05 21:22:54 by bahn             ###   ########.fr       */
+/*   Updated: 2022/04/18 19:37:28 by bahn             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 int	camera_move(int keycode, t_scene *scene)
 {
-	// printf("keycode : %d\n", keycode);
-	if (keycode == 65362)
+	printf("keycode : %d\n", keycode);
+	if (keycode == 65362 || keycode == 126)
 		// scene->camera.orig.y += 0.1;
 		scene->camera.dir.y += 0.1;
-	else if (keycode == 65364)
+	else if (keycode == 65364 || keycode == 125)
 		scene->camera.dir.y -= 0.1;
-	else if (keycode == 65361)
+	else if (keycode == 65361 || keycode == 123)
 		scene->camera.dir.x -= 0.1;
-	else if (keycode == 65363)
+	else if (keycode == 65363 || keycode == 124)
 		scene->camera.dir.x += 0.1;
 	else
 		return (0);
