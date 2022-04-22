6864555e57bf77cc7d70938641c9ed2707833968
jseol <jseol@student.42seoul.kr>
2022-04-22 15:47:28 +0900

    cy_tmp

--- srcs/object/cylinder/cylinder.c
+++ srcs/object/cylinder/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: jaeyu <jaeyu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 13:46:31 by bahn              #+#    #+#             */
-/*   Updated: 2022/04/22 15:24:48 by jaeyu            ###   ########.fr       */
+/*   Updated: 2022/04/22 15:45:55 by jaeyu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ t_vec3		get_cy_normal(t_cylinder *cy, t_hit_record *rec)
 
 t_bool		hit_cylinder_surface(t_cylinder *cy, t_ray *r, t_hit_record *rec)
 {
+/*
 	t_vec3		oc;
 	
 	double		a;
@@ -48,12 +49,12 @@ t_bool		hit_cylinder_surface(t_cylinder *cy, t_ray *r, t_hit_record *rec)
 	
 	oc = vsub(r->orig, cy->coord); // 카메라 원점에서 원기둥의 중심점까지의 방향 벡터
 
-/*
+
 	a = vlength2(vcross(r->dir, cy->dir));
 	half_b = vdot(vcross(r->dir, cy->dir), vcross(oc, cy->dir));
 	c = vlength2(vcross(oc, cy->dir)) - pow(cy->diameter / 2, 2.0);
 	discriminant = pow(half_b, 2.0) - a * c;
-*/
+
 
 	t_vec3		var_hc;
 	t_vec3		var_h;
@@ -102,43 +103,56 @@ t_bool		hit_cylinder_surface(t_cylinder *cy, t_ray *r, t_hit_record *rec)
 	rec->normal = get_cy_normal(cy, rec);
 	set_face_normal(r, rec); // 교점 법선 벡터와 광선의 방향 벡터는 항상 반대방향이어야한다.
 	return (TRUE);
-}
-
-/*
-t_bool		hit_cylinder_circle(t_cylinder *cy, t_ray *r, t_hit_record *rec, t_point3 center)
-{
-	double	denom;
-	t_vec3	oc;
-	double	t;
-
-	// 광선과 원기둥의 법선 벡터의 내적이 0. 즉, 평행이라면 광선은 윗면이나 아랫면과 교차하지않는다.
-	// 광선과 원기둥의 사이각이 윗면과 아랫면의 t를 구하기 위한 분모로 활용된다.
-	denom = vdot(r->dir, cy->dir);
-	if (denom == 0)
-		return (FALSE);
-	
-	oc = vsub(center, r->orig); // 광선의 원점에서 윗면 또는 아랫면의 중심점까지의 벡터
-	t = vdot(oc, cy->dir) / denom; // 윗면 또는 아랫면의 t : oc와 원기둥의 법선 벡터의 내적 값을 denom으로 나눈다.
-	
-	// 광원에서부터 t 만큼 떨어진 지점이 윗면 또는 아랫면의 중심점과의 거리가 원기둥의 반지름보다 높을 경우 광선과 원기둥의 윗면 또는 아랫면은 교차하지 않는다.
-	if (vlength(vsub(vsum(r->orig, vmul_t(t, r->dir)), center)) > cy->diameter / 2)
-		return (FALSE);
-		
-	// 윗면 또는 아랫면의 t와 tmin, tmax를 비교하여 최소/최대 거리 범위에 속하는지 검사한다.
-	if (t < rec->tmin || t > rec->tmax)
-		return (FALSE);
-	
-	if (t < rec->t) // 가장 작은 근 대입
-		rec->t = t;
-	else
-		return (TRUE); // 실질적인 교점은 아니지만 그림자의 면적을 고려하기 위해 TRUE를 반환한다.
-	rec->p = ray_at(r, rec->t);
-	rec->normal = cy->dir; // 원기둥의 법선 벡터를 교점의 법선 벡터에 그대로 대입
-	set_face_normal(r, rec); // 교점 법선 벡터와 광선의 방향 벡터는 항상 반대방향이어야한다.
-	return (TRUE);
-
-}
 */
+	t_vec3	h;
+	t_vec3	w;
+	t_vec3	v;
+	
+	double	a;
+	double	b;
+	double	c;
+	double	discriminant;
+	
+	double	root;
+	
+	h = vunit(vsub(cy->coord_top, cy->coord_bot));
+	w = vsub(r->orig, cy->coord_bot);
+	v = r->dir;
+	
+	a = vlength2(v) - pow(vdot(v, h), 2);
+	b = 2 * (vdot(v, w) - (vdot(v, h) * vdot(w, h)));
+	c = vlength2(w) - pow(vdot(w, h), 2) -  pow(cy->diameter / 2, 2);
+	
+	discriminant = pow(b, 2) - (4 * a * c);	
+	if (discriminant < 0)
+		return (FALSE);
+	root = (-b - sqrt(discriminant)) / (2 * a);
+	if (root < rec->tmin || root > rec->tmax)
+	{
+		root = (-b + sqrt(discriminant)) / (2 * a);
+		if (root < rec->tmin || root > rec->tmax)
+			return (FALSE);
+	}
+
+	// if (vdot(vsub(ray_at(r, root), cy->coord_top), cy->dir) > 0)
+	// 	return (FALSE);
+	// if (vdot(vsub(ray_at(r, root), cy->coord_bot), cy->dir) < 0)
+	// 	return (FALSE);
+
+	if (vdot(vsub(ray_at(r, root), cy->coord_bot), vsub(cy->coord_top, cy->coord_bot)) < 0)
+		return (FALSE);
+	if (vdot(vsub(ray_at(r, root), cy->coord_bot), vsub(cy->coord_top, cy->coord_bot)) > vlength2(vsub(cy->coord_top, cy->coord_bot)))
+		return (FALSE);
+
+	// if (vlength(vsub(ray_at(r, root), cy->coord)) > sqrt(pow(cy->diameter / 2, 2) + pow(cy->height / 2, 2)))
+	// 	return (FALSE);
+	rec->t = root;
+	rec->p = ray_at(r, root);
+	rec->normal = vunit(vsub(vsub(rec->p, cy->coord), vmul_t(vdot(cy->dir, vsub(rec->p, cy->coord)), cy->dir)));
+	rec->p = vsum(rec->p, vmul_t(EPSILON, rec->normal));
+	set_face_normal(r, rec);
+	return (TRUE);
+}
 
 t_bool		hit_cylinder_circle(t_cylinder *cy, t_ray *r, t_hit_record *rec, t_point3 circle_center)
 {
